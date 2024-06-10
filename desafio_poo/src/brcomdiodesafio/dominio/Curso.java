9a9a49574c184c50653d99a1e145630d349595ef
GuilhermeLino <joseguilhermelino3005@gmail.com>
2024-06-10 18:18:19 -0300

    Criando as classes mentoria e curso

--- desafio_poo/src/brcomdiodesafio/dominio/Curso.java
+++ desafio_poo/src/brcomdiodesafio/dominio/Curso.java
@@ -1,31 +1,15 @@
 package brcomdiodesafio.dominio;
 
-public class Curso {
-     private String titulo;
-     private String descricao;
+public class Curso extends Conteudo {
      private int cargaHoraria;
 
      public Curso(){
-          
+
      }
 
-     public String getTitulo() {
-          return titulo;
+     public double calcularXp() {
+          return XP_PADRAO * cargaHoraria;
      }
-
-     public void setTitulo(String titulo) {
-          this.titulo = titulo;
-     }
-
-    
-     public String getDescricao() {
-          return descricao;
-     }
-
-     public void setDescricao(String descricao) {
-          this.descricao = descricao;
-     }
-
 
      public int getCargaHoraria() {
           return cargaHoraria;
@@ -38,10 +22,13 @@ public class Curso {
     @Override
     public String toString() {
      return "Curso{" +
-             "titulo='" + titulo+ '\'' +
-             ", descricao ='" + descricao + '\'' +
+             "titulo='" + getTitulo()+ '\'' +
+             ", descricao ='" + getDescricao() + '\'' +
              ", cargaHoraria=" + cargaHoraria +
              '}';
      }
+
+
+
 
 }
--- desafio_poo/src/brcomdiodesafio/dominio/Mentoria.java
+++ desafio_poo/src/brcomdiodesafio/dominio/Mentoria.java
@@ -2,12 +2,16 @@ package brcomdiodesafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-     private String titulo;
-     private String descricao;
+public class Mentoria extends Conteudo {
      private LocalDate data;     
 
      public Mentoria(){
+
+     }
+
+     public double calcularXp(){
+          return XP_PADRAO + 20;
+
      }
 
      public LocalDate getData() {
@@ -18,28 +22,14 @@ public class Mentoria {
           this.data = data;
      }
 
-     public String getDescricao() {
-          return descricao;
-     }
-
-     public void setDescricao(String descricao) {
-          this.descricao = descricao;
-     }
-
-     public String getTitulo() {
-          return titulo;
-     }
-
-     public void setTitulo(String titulo) {
-          this.titulo = titulo;
-     }
-
      @Override
      public String toString() {
           return "Curso{" +
-          "titulo='" + titulo+ '\'' +
-          ", descricao ='" + descricao + '\'' +
+          "titulo='" +getTitulo()+ '\'' +
+          ", descricao ='" + getDescricao() + '\'' +
           ", data =" + data +
           '}';
      }
+     
+
 }
